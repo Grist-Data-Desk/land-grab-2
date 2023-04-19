3308943eb30364ec76d8b5ea8d3f4966d24ac4c3
tusharkh <33332821+tusharkh@users.noreply.github.com>
2023-04-19 13:50:42 -0700

    added scripts to download and clean shapefiles

--- query.py
+++ query.py
@@ -6,15 +6,20 @@ import geopandas as gpd
 import restapi
 import typer
 
-from config import QUERY_CONFIGS, STATE, UNIVERSITY, MANAGING_AGENCY, RIGHTS_TYPE, ATTRIBUTE_FILTER, COLUMNS
+from config import (SURFACE_ATTRIBUTE_LABEL_TO_FILTER_BY,
+                    SUBSURFACE_ATTRIBUTE_LABEL_TO_FILTER_BY, URL,
+                    ATTRIBUTE_LABEL_TO_FILTER_BY, ATTRIBUTE_CODE_TO_ALIAS_MAP,
+                    CONFIGS, STATE, UNIVERSITY, MANAGING_AGENCY, RIGHTS_TYPE,
+                    ATTRIBUTE_FILTER, COLUMNS, DOWNLOAD_TYPE,
+                    SHAPEFILE_DOWNLOAD_TYPE, API_QUERY_DOWNLOAD_TYPE, LAYER)
 
 
-def to_kebab_case(value):
+def to_kebab_case(string):
   '''convert string to kebab case'''
-  if '_' in value:
-    return "-".join(value.lower().split('_'))
+  if '_' in string:
+    return "-".join(string.lower().split('_'))
   else:
-    return "-".join(value.lower().split())
+    return "-".join(string.lower().split())
 
 
 def get_filename(state, label, alias, filetype):
@@ -22,9 +27,9 @@ def get_filename(state, label, alias, filetype):
   return f'{to_kebab_case(state)}-{to_kebab_case(label)}-{to_kebab_case(alias)}{filetype}'
 
 
-def get_query_data_directory(query_config):
+def get_queried_data_directory(query_config):
   state = query_config[STATE]
-  return f'query-data/{state}/'
+  return f'queried-data/{state}/'
 
 
 def get_cleaned_data_directory(query_config):
@@ -36,8 +41,10 @@ def get_state_abbreviation(api_source):
   return api_source.split('-')[0]
 
 
-def query_arcgis_restapi(query_config, api_source, label, value, directory,
-                         filename):
+def query_arcgis_restapi(query_config, api_source, label, code, alias,
+                         directory):
+  # create a descriptive filename to store query info
+  filename = get_filename(api_source, label, alias, '.geojson')
 
   # url for specific Map Server
   url = query_config['url']
@@ -54,7 +61,7 @@ def query_arcgis_restapi(query_config, api_source, label, value, directory,
                 indent=2)  # indent allows for pretty view
 
   # desired attribute conditions to filter the query by
-  attribute_filter = f'{label}={value}'
+  attribute_filter = f'{label}={code}'
 
   # then filter by specific attributes
   features = layer.query(where=attribute_filter,
@@ -68,56 +75,78 @@ def query_arcgis_restapi(query_config, api_source, label, value, directory,
   print(f'Found {len(features)} features with {attribute_filter}')
 
   # save geojson file, may save as json depending on the esri api version, needs 10.3 to saave as geojson
-  features.dump(directory + filename,
-                indent=2)  # indent allows for pretty view
+  features.dump(directory + filename, indent=2)  # indent allows for pretty view
   # features.dump('test.json', indent=2) # indent allows for pretty view
 
   # OR, you can save it directly to a shapefile (does not require arcpy)
   # layer.export_layer('test.shp', where=attribute_filter)
 
 
+def clean_queried_data(api_source, query_config, label, alias,
+                       queried_data_directory, cleaned_data_directory):
+
+  filename = get_filename(api_source, label, alias, '.json')
+  gdf = gpd.read_file(queried_data_directory + filename)
+
+  gdf = format_columns(gdf, query_config, label, alias)
+
+  filename = get_filename(api_source, label, alias, '.geojson')
+  gdf.to_file(cleaned_data_directory + filename, driver='GeoJSON')
+
+
+def format_columns(gdf, query_config, label, alias):
+  # add useful column data to dataset
+  gdf[STATE] = query_config[STATE]
+  gdf[UNIVERSITY] = query_config[UNIVERSITY]
+  gdf[MANAGING_AGENCY] = query_config[MANAGING_AGENCY]
+  if (query_config.get(RIGHTS_TYPE)):
+    gdf[RIGHTS_TYPE] = query_config[RIGHTS_TYPE]
+
+  # get the readable attribute filer which we used in the query
+  gdf[ATTRIBUTE_FILTER] = f'{label}={alias}'
+
+  # remove remaining columns
+  columns_to_drop = [column for column in gdf.columns if column not in COLUMNS]
+  return gdf.drop(columns_to_drop, axis=1)
+
+
 def main(api_source: str):
 
-  query_config = QUERY_CONFIGS[api_source]
+  query_config = CONFIGS[api_source]
 
-  # create the correct data directoies
-  query_data_directory = get_query_data_directory(query_config)
-  if not os.path.exists(query_data_directory):
-    os.makedirs(query_data_directory)
+  # create the correct data directories
+  queried_data_directory = get_queried_data_directory(query_config)
+  if not os.path.exists(queried_data_directory):
+    os.makedirs(queried_data_directory)
 
   cleaned_data_directory = get_cleaned_data_directory(query_config)
   if not os.path.exists(cleaned_data_directory):
     os.makedirs(cleaned_data_directory)
 
-  for label in query_config['attribute_label_to_filter_by']:
-    for value, alias in query_config['attribute_value_to_alias_map'].items():
-      # create a descriptive filename to store query info
-      filename = get_filename(api_source, label, alias, '.geojson')
+  if query_config[DOWNLOAD_TYPE] == API_QUERY_DOWNLOAD_TYPE:
 
-      query_arcgis_restapi(query_config, api_source, label, value,
-                           query_data_directory, filename)
+    for label in query_config[ATTRIBUTE_LABEL_TO_FILTER_BY]:
+      for code, alias in query_config[ATTRIBUTE_CODE_TO_ALIAS_MAP].items():
 
-      filename = get_filename(api_source, label, alias, '.json')
-      gdf = gpd.read_file(query_data_directory + filename)
+        query_arcgis_restapi(query_config, api_source, label, code, alias,
+                             queried_data_directory)
 
-      # add useful column data to dataset
-      gdf[STATE] = query_config[STATE]
-      gdf[UNIVERSITY] = query_config[UNIVERSITY]
-      gdf[MANAGING_AGENCY] = query_config[MANAGING_AGENCY]
-      if (query_config.get(RIGHTS_TYPE)):
-        gdf[RIGHTS_TYPE] = query_config[RIGHTS_TYPE]
+        clean_queried_data(api_source, query_config, label, alias,
+                           queried_data_directory, cleaned_data_directory)
 
-      # get the readable attribute filer which we used in the query
-      gdf[ATTRIBUTE_FILTER] = f'{label}={alias}'
+  elif query_config[DOWNLOAD_TYPE] == SHAPEFILE_DOWNLOAD_TYPE:
 
-      # remove remaining columns
-      columns_to_drop = [
-          column for column in gdf.columns if column not in COLUMNS
-      ]
-      gdf.drop(columns_to_drop, axis=1, inplace=True)
+    gdf = gpd.read_file(query_config[URL], layer=query_config.get(LAYER))
 
-      filename = get_filename(api_source, label, alias, '.geojson')
-      gdf.to_file(cleaned_data_directory + filename, driver='GeoJSON')
+    for label in query_config[ATTRIBUTE_LABEL_TO_FILTER_BY]:
+      for code, alias in query_config[ATTRIBUTE_CODE_TO_ALIAS_MAP].items():
+
+        gdf = gdf[gdf[label] == code].copy()
+
+        gdf = format_columns(gdf, query_config, label, alias)
+
+        filename = get_filename(api_source, label, alias, '.geojson')
+        gdf.to_file(cleaned_data_directory + filename, driver='GeoJSON')
 
 
 if __name__ == "__main__":
