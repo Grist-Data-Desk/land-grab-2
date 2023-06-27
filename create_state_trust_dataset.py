fb8cba86b039e82db8cd9353165d76f57c7aa904
Maria Rose <mpr@marias-air.lan>
2023-06-27 11:16:47 -0400

    university parser and ID match update, state trust dataset formatting change

--- create_state_trust_dataset.py
+++ create_state_trust_dataset.py
@@ -13,52 +13,52 @@ app = typer.Typer()
 
 
 def _queried_data_directory(state=None):
-  return state_specific_directory(STATE_TRUST_DIRECTORY + QUERIED_DIRECTORY,
-                                  state)
+    return state_specific_directory(STATE_TRUST_DIRECTORY + QUERIED_DIRECTORY,
+                                    state)
 
 
 def _cleaned_data_directory(state=None):
-  return state_specific_directory(STATE_TRUST_DIRECTORY + CLEANED_DIRECTORY,
-                                  state)
+    return state_specific_directory(STATE_TRUST_DIRECTORY + CLEANED_DIRECTORY,
+                                    state)
 
 
 def _merged_data_directory(state=None):
-  return state_specific_directory(STATE_TRUST_DIRECTORY + MERGED_DIRECTORY,
-                                  state)
+    return state_specific_directory(STATE_TRUST_DIRECTORY + MERGED_DIRECTORY,
+                                    state)
 
 
 @app.command()
 def merge_single_state(state: str):
-  cleaned_data_directory = _cleaned_data_directory(state)
-  merged_data_directory = _merged_data_directory(state)
-  merge_single_state_helper(state, cleaned_data_directory,
-                            merged_data_directory)
+    cleaned_data_directory = _cleaned_data_directory(state)
+    merged_data_directory = _merged_data_directory(state)
+    merge_single_state_helper(state, cleaned_data_directory,
+                              merged_data_directory)
 
 
 @app.command()
 def merge_all_states():
-  # first, find all states for which we have cleaned data
-  cleaned_data_directory = _cleaned_data_directory()
-  merged_data_directory = _merged_data_directory()
-  merge_all_states_helper(cleaned_data_directory, merged_data_directory)
+    # first, find all states for which we have cleaned data
+    cleaned_data_directory = _cleaned_data_directory()
+    merged_data_directory = _merged_data_directory()
+    merge_all_states_helper(cleaned_data_directory, merged_data_directory)
 
 
 @app.command()
 def extract_and_clean_all():
-  for source in STATE_TRUST_CONFIGS.keys():
-    extract_and_clean_single_source(source)
+    for source in STATE_TRUST_CONFIGS.keys():
+        extract_and_clean_single_source(source)
 
 
 @app.command()
 def extract_and_clean_single_source(source: str):
-  # get state and correct config
-  config = STATE_TRUST_CONFIGS[source]
-  state = config[STATE]
-  queried_data_directory = _queried_data_directory(state)
-  cleaned_data_directory = _cleaned_data_directory(state)
-  extract_and_clean_single_source_helper(source, config, queried_data_directory,
-                                         cleaned_data_directory)
+    # get state and correct config
+    config = STATE_TRUST_CONFIGS[source]
+    state = config[STATE]
+    queried_data_directory = _queried_data_directory(state)
+    cleaned_data_directory = _cleaned_data_directory(state)
+    extract_and_clean_single_source_helper(source, config, queried_data_directory,
+                                           cleaned_data_directory)
 
 
 if __name__ == "__main__":
-  app()
+    app()
