4a3fe57f6d8efc8baaea412b2abe9fe240a2ae2c
Maria Parazo Rose <mrose@grist.org>
2023-08-16 13:52:09 -0400

    added 8 parsers for the universities that provided parcel ID lists

---
+++ land_grab/university_real_estate/parcel_list_parsers/oh_ohiostateuniversity_parser.py
@@ -0,0 +1,3 @@
+def ohos_parser(l):
+  # column 1 or [0], take out - and spaces, concatenate
+  # ignore header cell (ParcelNo)
---
+++ land_grab/university_real_estate/parcel_list_parsers/tn_universityoftennessee_parser.py
@@ -0,0 +1,5 @@
+def tnut_parser(l):
+  # take out spaces and concatenate
+  # parcel code column 3 (or [2])
+  # ignore header cell
+  pass
