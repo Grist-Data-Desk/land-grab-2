11b6bc2efba0f0c60d538032f95677f5e8a892be
Maria Parazo Rose <mrose@grist.org>
2023-08-16 16:17:24 -0400

    filling out university parsers

--- land_grab/university_real_estate/parcel_list_parsers/ca_universityofcalifornia_parser.py
+++ land_grab/university_real_estate/parcel_list_parsers/ca_universityofcalifornia_parser.py
@@ -1,2 +1,9 @@
 def cauc_parser(l):
+  # take out - and spaces, concatenate
+  # commas and semicolons are usually delimiters, but sometimes it separates the last few digits, which
+  # technically are like additional parcel IDs, with a slight change at the end
+  # sometimes it has "or" to also indicate new ID combo with the last few digits being different
+  # weird adjustments need to be made for some of the strings (string descriptions include 'and', further desc. etc)
+  # return cells with weird strings for additional parsing
+
   pass
