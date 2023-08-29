298681b5bd1a1b6d48d8f468e3b4c88379a43ca8
Marcelle Bonterre <laanak@gmail.com>
2023-08-29 10:15:40 -0400

    WIP

--- scripts/db/populate_regrid_table.py
+++ scripts/db/populate_regrid_table.py
@@ -1,12 +1,17 @@
 import atexit
-import concurrent.futures
 import json
 import logging
 import os
 import shutil
 from pathlib import Path
 
+import dask
+import dask.bag
 import pandas as pd
+from dask.diagnostics import ProgressBar
+from tqdm import tqdm
+
+from land_grab.db.gen_regrid_table_def import regrid_schema_to_grist_table
 
 logging.getLogger('pysftp').setLevel(logging.WARNING)
 logging.getLogger('paramiko').setLevel(logging.WARNING)
@@ -18,15 +23,31 @@ try:
 except:
     pass
 import pysftp
-from tqdm import tqdm
 
 from land_grab.db.gristdb import GristDB, GristDbIndexType
-from land_grab.db.tables import REGRID_TABLE
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
 log = logging.getLogger(__name__)
-
 FAILED_ZIP = []
+
+REGRID_TABLE = regrid_schema_to_grist_table(Path('~/Downloads/regrid_parcel_schema.csv').expanduser())
+
+
+def custom_obj_parse(o):
+    if isinstance(o, list):
+        return json.dumps(o)
+
+    if 'geometry' in o and 'properties' in o:
+        o['properties']['geometry'] = json.dumps(o['geometry']['coordinates'])
+        return o['properties']
+
+    if 'coordinates' in o:
+        o['coordinates'] = json.dumps(o['coordinates'])
+
+    if 'geometry' not in o and 'coordinates' not in o and 'geoid' not in o:
+        return json.dumps(o)
+
+    return o
 
 
 def insert_geojson(zip_name, zip_path_local, retry=20):
@@ -37,11 +58,12 @@ def insert_geojson(zip_name, zip_path_local, retry=20):
         return
 
     try:
-        records = []
-        for feature in geojson['features']:
-            record = feature['properties']
-            record['geometry'] = json.dumps(feature['geometry']['coordinates'])
-            records.append(record)
+        features_as_json = json.dumps(geojson['features'])
+        records = json.loads(features_as_json,
+                             object_hook=custom_obj_parse,
+                             parse_float=str,
+                             parse_int=str,
+                             parse_constant=str)
 
         GristDB().copy_to(REGRID_TABLE, records)
     except Exception as err:
@@ -76,9 +98,7 @@ def unzip_to_json(zip_path_local: Path):
             pass
 
 
-def upload(zip_remote_path: str):
-    log.info('Worker working')
-
+def upload(zip_remote_path: Path):
     ftp_url = 'sftp.regrid.com'
     uname = 'grist'
     pword = 'sweeping-clamour-beheld-mesmeric'
@@ -90,9 +110,17 @@ def upload(zip_remote_path: str):
             f'/Users/marcellebonterre/Projects/land-grab-2/scripts/zips/{zip_remote_path.name}'
         ).resolve()
         sftp.get(str(zip_remote_path), str(zip_path_local))
-        insert_geojson(zip_remote_path.name, zip_path_local)
+
+    # MUST be outside sftp connection
+    insert_geojson(zip_remote_path.name, zip_path_local)
 
     return True
+
+
+def in_dask(batch, batch_size):
+    with dask.config.set(scheduler='processes'):
+        with ProgressBar():
+            dask.bag.from_sequence(batch, partition_size=batch_size).map(upload).compute(scheduler='processes')
 
 
 def main():
@@ -100,38 +128,21 @@ def main():
     df = pd.read_csv(ziplist)
     zipnames = df.zip_remote_path.tolist()
 
-    batch_size = 10
+    zips_dir = Path(f'/Users/marcellebonterre/Projects/land-grab-2/scripts/zips')
+    if not zips_dir.exists():
+        zips_dir.mkdir(parents=True, exist_ok=True)
+
+    batch_size = 25
     batches = [zipnames[i:i + batch_size] for i in range(0, len(zipnames), batch_size)]
     for batch in tqdm(batches):
-        with concurrent.futures.ProcessPoolExecutor(max_workers=batch_size) as t_pool:
-            fs = [t_pool.submit(upload, z) for z in batch]
-            for f in concurrent.futures.as_completed(fs):
-                f.result()
+        in_dask(batch, batch_size=5)
 
-    try:
-        log.info('preparing to create database indexes')
-        db = GristDB()
-
-        log.info('Creating database index on id column')
-        db.create_index('regrid', 'id')
-
-        log.info('Creating database index on parcelnumb column')
-        db.create_index('regrid', 'parcelnumb')
-
-        log.info('Creating database index on parcelnumb_no_formatting column')
-        db.create_index('regrid', 'parcelnumb_no_formatting')
-
-        log.info('Creating database index on owner column')
-        db.create_index('regrid', 'owner', type=GristDbIndexType.TEXT)
-
-        log.info('Creating database index on mailadd column')
-        db.create_index('regrid', 'mailadd', type=GristDbIndexType.TEXT)
-
-        log.info('done creating database indexes')
-        log.info('Current database indexes listing:')
-        log.info(db.list_indexes())
-    except Exception as err:
-        log.error(err)
+    # for batch in tqdm(batches):
+    #     with concurrent.futures.ProcessPoolExecutor(max_workers=batch_size) as t_pool:
+    #         fs = [t_pool.submit(upload, z) for z in batch]
+    #         for f in concurrent.futures.as_completed(fs):
+    #             f.result()
+    # for z in batches[0]: upload(z)  # DEBUG ONLY
 
 
 def write_fails():
