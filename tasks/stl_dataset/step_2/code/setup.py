70d84501913a7d5437e891818e46e97ce0cc3870
Marcelle Bonterre <laanak@gmail.com>
2023-09-18 13:31:47 -0400

    masive repo organization refactor + move to pyproject.toml

--- tasks/stl_dataset/step_2/code/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import find_packages, setup  # or find_namespace_packages
-
-setup(
-    packages=find_packages(),
-    entry_points={'console_scripts': ['run = land_activity_search:activity_match.run']}
-)
