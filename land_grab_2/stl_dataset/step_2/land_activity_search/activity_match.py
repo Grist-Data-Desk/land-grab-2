baf80762078c0ea21b4792aae5a79ce7543c3fff
Marcelle Bonterre <laanak@gmail.com>
2023-10-16 11:15:18 -0400

    activity match

--- land_grab_2/stl_dataset/step_2/land_activity_search/activity_match.py
+++ land_grab_2/stl_dataset/step_2/land_activity_search/activity_match.py
@@ -8,9 +8,10 @@ import geopandas
 import numpy as np
 from tqdm import tqdm
 
+from land_grab_2.init_database.db.gristdb import GristDB
 from land_grab_2.stl_dataset.step_2.land_activity_search.state_data_sources import STATE_ACTIVITIES, rewrite_rules
+from land_grab_2.utilities.overlap import eval_overlap_keep_left, STATE_LONG_NAME
 from land_grab_2.utilities.utils import GristCache
-from land_grab_2.utilities.overlap import eval_overlap_keep_left
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -155,12 +156,12 @@ def get_activity_name(state, activity, activity_row):
 
 
 def extract_matches(state, activity, activity_data, grist_data, overlap_report):
-    for name, group in overlap_report.groupby("joinidx_0")[["joinidx_1"]]:
-        activity_row = activity_data[activity_data['joinidx_0'] == name]
+    for name, group in overlap_report.groupby("joinidx_1")[["joinidx_0"]]:
+        activity_row = activity_data[activity_data['joinidx_1'] == name]
         activity_name = get_activity_name(state, activity, activity_row)
 
-        grist_row_idxs = group['joinidx_1'].tolist()
-        grist_rows = grist_data[grist_data['joinidx_1'].isin(grist_row_idxs)].index.tolist()
+        grist_row_idxs = group['joinidx_0'].tolist()
+        grist_rows = grist_data[grist_data['joinidx_0'].isin(grist_row_idxs)].index.tolist()
         for g_row_idx in grist_rows:
             if activity_name:
                 try:
@@ -172,9 +173,72 @@ def extract_matches(state, activity, activity_data, grist_data, overlap_report):
                 GRIST_DATA_UPDATE[g_row_idx].add(activity_name)
 
 
+def geodf_overlap_cmp_keep_left(left, right):
+    try:
+        if not 'joinidx_1' in left.index:
+            left["joinidx_1"] = np.arange(0, left.shape[0]).astype(int).astype(str)
+
+        right.drop_duplicates([
+            c
+            for c in right.columns
+            if 'object' not in c
+        ], inplace=True)
+        right["joinidx_0"] = np.arange(0, right.shape[0]).astype(int)
+
+        right = right.to_crs(left.crs)
+        overlapping_regions = geopandas.sjoin(right[['joinidx_0', 'geometry']],
+                                              left[['joinidx_1', 'geometry']],
+                                              how="left",
+                                              predicate='intersects').dropna()
+        return overlapping_regions
+    except Exception as err:
+        try:
+            right = right.set_crs(left.crs).to_crs(left.crs)
+            return geodf_overlap_cmp_keep_left(left, right)
+        except Exception as err:
+            print(f'failing on mysterious except')
+            assert 1
+
+
 def find_overlaps(state, activity, activity_data, grist_data):
     try:
-        overlapping_regions, left, right = eval_overlap_keep_left(activity_data, grist_data, return_inputs=True)
+        overlapping_regions = geodf_overlap_cmp_keep_left(activity_data, grist_data)
+
+        return (extract_matches(state, activity, activity_data, grist_data, overlapping_regions)
+                if overlapping_regions.shape[0] > 0
+                else [])
+    except Exception as err:
+        print(f'failing on mysterious except in find_overlaps()')
+
+
+def extract_matches_latest(state, activity, activity_data, grist_data, overlap_report):
+    for name, group in overlap_report.groupby("joinidx_1")[["joinidx_0"]]:
+        activity_row = activity_data[activity_data['joinidx_1'] == name]
+        activity_name = get_activity_name(state, activity, activity_row)
+
+        # grist_row_idxs = group['joinidx_1'].tolist()
+        grist_rows = grist_data[grist_data['joinidx_0'].isin(group['joinidx_0'].tolist())].index.tolist()
+        for g_row_idx in grist_rows:
+            if activity_name:
+                try:
+                    existing_activity = grist_data.at[g_row_idx, 'activity']
+                    if existing_activity is not None:
+                        assert 1
+                except Exception as err:
+                    assert 1
+                GRIST_DATA_UPDATE[g_row_idx].add(activity_name)
+
+
+def find_overlaps_latest(state, activity, activity_data, grist_data):
+    try:
+        crs_list = GristDB().crs_search_by_state(STATE_LONG_NAME[state])
+        crs_list = [f"{r['data_source']}:{r['coord_ref_sys_code']}" for r in crs_list]
+        crs_list = [grist_data.crs] + crs_list
+
+        overlapping_regions, left, right = eval_overlap_keep_left(activity_data,
+                                                                  grist_data,
+                                                                  crs_list=crs_list,
+                                                                  return_inputs=True)
         activity_data, grist_data = left, right
 
         return (extract_matches(state, activity, activity_data, grist_data, overlapping_regions)
@@ -188,6 +252,11 @@ def match_all_activities(states_data=None, grist_data=None):
     log.info(f'processing states {states_data.keys()}')
 
     for activity_state, activity_info in states_data.items():
+        print(f'state: {activity_state}')
+        if 'UT' in activity_state:
+            print('skipping utah')
+            continue
+
         if not activity_info:
             log.error(f'NO ACTIVITY CONFIG FOR {activity_state}')
             continue
@@ -219,21 +288,25 @@ def main(stl_path: Path, the_out_dir: Path):
     COLUMN_RENAME_RULES = rewrite_rules
 
     log.info(f'reading {stl_path}')
-    gdf = GristCache(f'{stl_path}').cache_read('stl_file', '.feather')
-    if gdf is None:
-        gdf = geopandas.read_file(str(stl_path))
-        GristCache(f'{stl_path}').cache_write(gdf, 'stl_file', '.feather')
+    gdf = geopandas.read_file(str(stl_path))
+
+    cols = gdf.columns.tolist()
+    rights_type_idx = cols.index('rights_type')
+    cols.insert(rights_type_idx + 1, 'activity')
 
     match_all_activities(STATE_ACTIVITIES, gdf)
     for row_idx, activity_list in GRIST_DATA_UPDATE.items():
         # gdf.at[row_idx, 'activity'] = ','.join(activity_list)
-        sorted_activities = list(sorted(list((activity_list))))
+        sorted_activities = list(sorted(list(activity_list)))
         gdf.loc[row_idx, 'activity'] = ','.join(sorted_activities)
 
     if 'joinidx_1' in gdf.columns:
         gdf.drop('joinidx_1', inplace=True, axis=1)
     if 'joinidx_0' in gdf.columns:
         gdf.drop('joinidx_0', inplace=True, axis=1)
+
+    # reorder cols
+    gdf = gdf[cols]
 
     log.info(f'final grist_data row_count: {gdf.shape[0]}')
     gdf.to_csv(str(the_out_dir / 'updated_grist_stl.csv'), index=False)
@@ -256,12 +329,8 @@ def run():
     global STL_COMPARISON_BASE_DIR, CACHE_DIR
     CACHE_DIR = base_data_dir / 'input/cache'
     STL_COMPARISON_BASE_DIR = base_data_dir / 'input/stl_activity_layers'
+    GristCache('', CACHE_DIR)  # DO NOT REMOVE unless willing to hunt & remove all transitive uses of GristCache
 
-    # set cache dir
-    GristCache('', CACHE_DIR)
-
-    # stl = base_data_dir / 'input/230815_nationals_STLs/0815_national_stls_deduplicated.geojson'
-    # stl = base_data_dir / 'input/all-states.geojson'
     step_1_data_directory = Path(f'{data_tld}/stl_dataset/step_1').resolve()
     stl = step_1_data_directory / 'input/state_trust/merged/all-states.geojson'
 
