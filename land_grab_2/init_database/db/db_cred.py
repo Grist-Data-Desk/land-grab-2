7b034e61b676d4a6e258c19f5843af98fbc29c98
clayton-aldern <clayton.aldern@gmail.com>
2024-02-22 10:32:21 -0800

    add missing files

---
+++ land_grab_2/init_database/db/db_cred.py
@@ -0,0 +1,7 @@
+DB_CREDS = {
+    'user': 'doadmin',
+    'password': 'AVNS_cZJUMVcRuKCMCfWfuZQ',
+    'host': 'grist-db-main-do-user-13109158-0.b.db.ondigitalocean.com',
+    'port': 25060,
+    'dbname': 'defaultdb'
+}
